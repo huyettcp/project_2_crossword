e32abdf552ff6f252e5d1a8fa5d8e53e2240f04c
huyettcp <huyettcp@gmail.com>
2013-10-13 19:54:01 -0400

    Removed image_url column from words table

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -3,23 +3,18 @@ require 'spec_helper'
 describe Word do
   before :each do 
     @word = Word.new(name: "Beer", image_url: "google")
+  end
 
-   end
-
-
-
-
-   describe "#name" do
-     it "should return the correct word" do
+  describe "#name" do
+    it "should return the correct word" do
        @word.name.should eq("Beer")
-     end
-   end
- end
+    end
+  end
 
-#   describe "#image_url" do
-#     it "should return the correct url" do
-#       @name.image_url.should eq ("google")
-#     end
-#   end
+  describe "#image_url" do
+    it "should return the correct url" do
+      @word.image_url.should eq ("google")
+    end
+  end
 
-# end
+end
