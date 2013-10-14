cc79cff3a403f736bda2cde33c4cd9608fc0e40f
huyettcp <huyettcp@gmail.com>
2013-10-14 12:32:22 -0400

    Test on models pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131014141413) do
+ActiveRecord::Schema.define(:version => 20131014162637) do
 
   create_table "photos", :force => true do |t|
     t.string   "url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "photos_and_words", :id => false, :force => true do |t|
-    t.integer "photo_id"
-    t.integer "word_id"
+    t.integer  "word_id"
   end
 
   create_table "words", :force => true do |t|
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -23,8 +23,8 @@
 
 describe Word do 
   it { validate_presence_of(:name) }
+  it { should allow_value("coffee").for(:name) }
   it { validate_presence_of(:photo_id) }
   it { should have_many(:photos) }
-
 end
 
