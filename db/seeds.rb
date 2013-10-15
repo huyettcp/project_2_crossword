b767009733159bc95cc48b337c7c37346161b6ee
adammcc <ajm845@gmail.com>
2013-10-15 09:36:24 -0400

    weaver is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ while img_array_index < count
 
 	photos = HTTParty.get("http://api.flickr.com/services/rest/?format=json&sort=random&method=flickr.photos.search&tags=#{img_array[img_array_index]}&tag_mode=all&api_key=0e2b6aaf8a6901c264acb91f151a3350&nojsoncallback=1")
 	sleep(2)	
-		puts photos['photos']['photo'].length
+		# puts photos['photos']['photo'].length
 			i = 0
 			temp_word = Word.create(name: img_array[img_array_index])
 			unless photos.empty?
