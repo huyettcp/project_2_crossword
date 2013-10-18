2ab45d4a340430e0abb10a14c944b8e9329dbbec
adammcc <ajm845@gmail.com>
2013-10-18 12:14:52 -0400

    fade is back

--- app/assets/javascripts/progress_bar.js
+++ app/assets/javascripts/progress_bar.js
@@ -1,44 +1,44 @@
-// onLoad = function() {
-//   // Cache selector for the progress bar
-//   var $progress_bar = $('#progress-bar');
-//   // Initialize our loading progress counter
-//   var done = 0;
-//   // Grab all the images
-//   var $pics = $('td');
-//   // console.log($pics);
-//   // Hide all the image tags
-//   $pics.hide();
-//   // Loop through each image and custom load their sources
+onLoad = function() {
+  // Cache selector for the progress bar
+  var $progress_bar = $('#progress-bar');
+  // Initialize our loading progress counter
+  var done = 0;
+  // Grab all the images
+  var $pics = $('td');
+  // console.log($pics);
+  // Hide all the image tags
+  $pics.hide();
+  // Loop through each image and custom load their sources
 
-//   var count = 0
-//   $pics.each(function() {
-//     // Cache the 'this' selector for use here and in the event handler
-//     var $pic = $(this);
+  var count = 0
+  $pics.each(function() {
+    // Cache the 'this' selector for use here and in the event handler
+    var $pic = $(this);
 
-//     // Grab the image source from our HTML img tag
-//     var src = $pic.css('background-image');
+    // Grab the image source from our HTML img tag
+    var src = $pic.css('background-image');
    
-//     var clean_src = $.trim(src.replace('url(','').replace(')',''));
+    var clean_src = $.trim(src.replace('url(','').replace(')',''));
  	
-//  	if(clean_src != '' && clean_src != "none") {
-//  		count++;
-// 	    $pic.css('background-image','');
-// 	    // Create a temporary img tag in memory (i.e. document.createElement)
-// 	    var $pic_tag = $('<img>');
-// 	    // Give our temporary img tag a source, which starts loading the image
-// 	    $pic_tag.attr('src', clean_src);
-// 	    // Add an event handler for when the img src load is complete (browser has downloaded the full image source)
-// 	    // i.e. $(document).ready() for an image tag
-// 	    $pic_tag.load(function() {
-// 	      // Increment our progress counter
-// 		    $pic.css('background-image', src);
-// 	        done++;
+ 	if(clean_src != '' && clean_src != "none") {
+ 		count++;
+	    $pic.css('background-image','');
+	    // Create a temporary img tag in memory (i.e. document.createElement)
+	    var $pic_tag = $('<img>');
+	    // Give our temporary img tag a source, which starts loading the image
+	    $pic_tag.attr('src', clean_src);
+	    // Add an event handler for when the img src load is complete (browser has downloaded the full image source)
+	    // i.e. $(document).ready() for an image tag
+	    $pic_tag.load(function() {
+	      // Increment our progress counter
+		    $pic.css('background-image', src);
+	        done++;
 	      
-// 	      // Update the progress bar
-// 	      updateProgress();
-// 	    });
-// 	}
-//   });
+	      // Update the progress bar
+	      updateProgress();
+	    });
+	}
+  });
 
   function updateProgress() {
     // If we've loaded all the images
