4c4999aab271c074ca5f9eb339038b562d5bfd5a
adammcc <ajm845@gmail.com>
2013-10-26 22:20:12 -0400

    darken screen works

--- app/assets/javascripts/qtip.js
+++ app/assets/javascripts/qtip.js
@@ -1,3 +1,5 @@
+var top_scores
+
 cleanUrl = function(_this) {
 	var imageUrl = $(_this).css('background-image');
 	var clean_imageUrl = imageUrl.replace('url(','').replace(')','');
@@ -64,41 +66,55 @@ $(document).on('mouseover', ".info", function(){
 	var _this = this;
 	$(this).qtip({
 	   overwrite: false,
-	   content: '<p>Do I reallly have to tell you how to tell you how to solve a crossword puzzle? </p>',
-	    style: {
-        classes: 'qtip-dark qtip-rounded'
-  },
-	  show: {
-      		ready: true
-    	}
-	})
-});
-
-$(document).on('mouseover', ".high_score", function(){
-  highScores();
-
-  var _this = this;
-	$(this).qtip({
-	   overwrite: false,
-	   content: "<p>" + top_scores[0].game_score + " " + top_scores[0].user_name + "</p> <br>" + "<p>" + top_scores[1].game_score + " " + top_scores[1].user_name + "</p> <br>" + "<p>" + top_scores[2].game_score + " " + top_scores[2].user_name + "</p>",
-	    style: {
-          classes: 'qtip-dark .qtip-content'
+	   content: '<div><p>Instructions coming</p><br><p>soon</p></div>',
+	   style: {
+          classes: 'qtip-light .qtip-content'
       },
-	   show: {
-      		ready: true
-    	}
+	  show: {
+      	  ready: true
+    	},
+      position: {
+          my: 'center',
+          at: 'center',
+          target: $(window)
+        },
+       show: {
+             modal: true
+         },
+       hide: true
 	})
 });
 
-var top_scores
-
-highScores = function() {
+$(function() {
   $.ajax({
     url: '/highscores',
     method: 'GET',
     dataType: 'json',
     }).done(function(data){
         top_scores = data
-    });
-    
-};
+    }); 
+});
+
+$(document).on('mouseover', ".high_score", function() {
+  console.log(top_scores);
+  var _this = this;
+	$(this).qtip({
+	   overwrite: false,
+	   content: "<p>" + top_scores[0].game_score + " " + top_scores[0].user_name + "</p> <br> <p>" + top_scores[1].game_score + " " + top_scores[1].user_name + "</p> <br> <p>" + top_scores[2].game_score + " " + top_scores[2].user_name + "</p>",
+	   style: {
+          classes: 'qtip-light .qtip-content'
+      },
+	   show: {
+      	  ready: true
+    	},
+       position: {
+          my: 'center',
+          at: 'center',
+          target: $(window)
+        },
+       show: {
+          modal: true
+        },
+       hide: true
+	})
+});
