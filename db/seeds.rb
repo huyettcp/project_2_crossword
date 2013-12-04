013f0103783707b6ab22a880d9c87dbc2c94bf14
adammcc <ajm845@gmail.com>
2013-12-04 16:25:12 -0500

    fix seed file url

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ img_array_index = 0
 
 while img_array_index < count
 
-	photos = HTTParty.get("http://api.flickr.com/services/rest/?format=json&sort=random&method=flickr.photos.search&tags=#{:tag]}&tag_mode=all&api_key=b7a1006111c8226670d353f33d2d7b9c&nojsoncallback=1")
+	photos = HTTParty.get("http://api.flickr.com/services/rest/?format=json&sort=random&method=flickr.photos.search&tags=#{:tag}&tag_mode=all&api_key=b7a1006111c8226670d353f33d2d7b9c&nojsoncallback=1")
 	
 			i = 0
 			temp_word = Word.create(name: img_array[img_array_index])
